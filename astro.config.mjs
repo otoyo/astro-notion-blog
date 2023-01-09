125890f33856b8f56bf27a60e6b17733f7e5b18a
otoyo <hiroki.t.1988@gmail.com>
2023-01-09 18:13:37 +0900

    Set site to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+
+const CUSTOM_DOMAIN = '' // <- Set your costom domain if you have. e.g. alpacat.com
+
+
+const getSite = function() {
+  if (!process.env.CF_PAGES) {
+    return 'http://localhost:3000'
+  }
+
+  if (process.env.CF_PAGES_BRANCH !== 'main') {
+    return process.env.CF_PAGES_URL
+  }
+
+  if (CUSTOM_DOMAIN) {
+    return `https://${CUSTOM_DOMAIN}`
+  }
+
+  return `https://${new URL(process.env.CF_PAGES_URL).host.split('.').slice(1).join('.')}`
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: getSite(),
+})
