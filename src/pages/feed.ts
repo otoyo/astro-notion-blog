dc46da6c5bd17949953e5620dc6326527b3105d5
otoyo <hiroki.t.1988@gmail.com>
2023-01-21 23:06:06 +0900

    Define feed

---
+++ src/pages/feed.ts
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss'
+import {
+  PUBLIC_SITE_TITLE,
+  PUBLIC_SITE_DESCRIPTION,
+} from '../server-constants'
+import { getAllPosts } from '../lib/notion/client'
+import { getPostLink } from '../lib/blog-helpers'
+
+export async function get() {
+  const posts = await getAllPosts()
+
+  return rss({
+    title: PUBLIC_SITE_TITLE,
+    description: PUBLIC_SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      link: new URL(getPostLink(post.Slug), import.meta.env.SITE).toString(),
+      title: post.Title,
+      pubDate: new Date(post.Date),
+    })),
+  })
+}
