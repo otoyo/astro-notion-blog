f0b1f5e44f91b42b6a4f5781d12bd805f32623a9
cross <26484083+crossn@users.noreply.github.com>
2023-01-22 23:39:07 +0900

    本家 Fix blob fetch #5 を反映

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -1,4 +1,4 @@
-import { Base64 } from 'js-base64'
+import fetch from 'node-fetch'
 import type {
   Heading1,
   Heading2,
@@ -6,19 +6,19 @@ import type {
   RichText,
 } from './interfaces'
 
-export const fetchImageAsDataURI = async (urlString: string): Promise<string> => {
-  try {
-    new URL(urlString)
-  } catch (err) {
-    console.log(err, urlString)
-    return Promise.resolve('')
+export const fetchImageAsDataURI = async (url: string): Promise<string | null> => {
+  const res = await fetch(url)
+  if (!res || !res.body) {
+    return Promise.resolve(null)
   }
+  const stream = res.body
 
-  const res = await fetch(urlString)
-  const blob = await res.blob()
-  const arrayBuffer = await blob.arrayBuffer()
-  const bin = String.fromCharCode(...new Uint8Array(arrayBuffer))
-  return `data:image/gif;base64,${Base64.btoa(bin)}`
+  return new Promise((resolve, reject) => {
+    const chunks: Buffer[] = []
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
+    stream.on('error', (err) => reject(err))
+    stream.on('end', () => resolve(`data:image/gif;base64,${Buffer.concat(chunks).toString('base64')}`))
+  })
 }
 
 export const getPostLink = (slug: string) => {
