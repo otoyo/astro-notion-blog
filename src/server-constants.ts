01a64a0a5d0289cea1778335468c9737f53370b5
herohoro <mtwd.office.25@gmail.com>
2023-01-23 14:39:02 +0900

    Defile profilePage

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -3,6 +3,7 @@ export const DATABASE_ID = import.meta.env.DATABASE_ID;
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID;
 export const NUMBER_OF_POSTS_PER_PAGE = 10;
 export const INDEX_PAGE_ID = import.meta.env.INDEX_PAGE_ID;
+export const PROFILE_PAGE_ID = import.meta.env.PROFILE_PAGE_ID;
 
 export const PUBLIC_SITE_TITLE = "I love astro-chan";
 export const PUBLIC_SITE_DESCRIPTION =
