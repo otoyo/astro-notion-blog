920ddcce2992fd1c4d3f06c7382ae36226b6fda5
imbeermug <imbeermug@gmail.com>
2023-01-30 20:26:08 +0900

    Changed blog title and home page description

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -3,5 +3,5 @@ export const DATABASE_ID = import.meta.env.DATABASE_ID
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID
 export const NUMBER_OF_POSTS_PER_PAGE = 10
 
-export const PUBLIC_SITE_TITLE = 'astro-notion-blog'
-export const PUBLIC_SITE_DESCRIPTION = 'astro-notion-blog is generated statically by Astro'
+export const PUBLIC_SITE_TITLE = 'びあまぐ荘'
+export const PUBLIC_SITE_DESCRIPTION = 'びあまぐ @imbeermug の個人ブログです。'
