31e2fe870237d4a416318f0b0aeb33431fde0ece
otoyo <hiroki.t.1988@gmail.com>
2023-02-18 15:21:24 +0900

    Set BASE_PATH in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
 import { defineConfig } from 'astro/config';
+import { BASE_PATH } from './src/server-constants';
 import OGImageUploader from './src/integrations/og-image-uploader';
 
 const CUSTOM_DOMAIN = ''; // <- Set your costom domain if you have. e.g. alpacat.com
 
 const getSite = function () {
   if (!process.env.CF_PAGES) {
-    return 'http://localhost:3000';
+    return new URL(BASE_PATH, 'http://localhost:3000').toString();
   }
 
   if (process.env.CF_PAGES_BRANCH !== 'main') {
-    return process.env.CF_PAGES_URL;
+    return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
   }
 
   if (CUSTOM_DOMAIN) {
-    return `https://${CUSTOM_DOMAIN}`;
+    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`);
   }
 
-  return `https://${new URL(process.env.CF_PAGES_URL).host
-    .split('.')
-    .slice(1)
-    .join('.')}`;
+  return new URL(
+    BASE_PATH,
+    `https://${new URL(process.env.CF_PAGES_URL).host
+      .split('.')
+      .slice(1)
+      .join('.')}`
+  ).toString();
 };
 
 // https://astro.build/config
 export default defineConfig({
   site: getSite(),
+  base: BASE_PATH,
   integrations: [OGImageUploader()],
 });
