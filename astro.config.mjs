9001c40f78950a6028d86c43465516de6e43df8e
otoyo <hiroki.t.1988@gmail.com>
2023-02-19 14:27:55 +0900

    Define and use PublicNotionCopier

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
 import OGImageUploader from './src/integrations/og-image-uploader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+import PublicNotionCopier from './src/integrations/public-notion-copier';
 
 const getSite = function () {
   if (!process.env.CF_PAGES) {
@@ -29,5 +30,5 @@ const getSite = function () {
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [OGImageUploader(), FeaturedImageDownloader()],
+  integrations: [OGImageUploader(), FeaturedImageDownloader(), PublicNotionCopier()],
 });
