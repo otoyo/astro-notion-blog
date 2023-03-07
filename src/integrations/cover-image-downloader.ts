633e7ac61a75fb8943be6aa51164b38f74fcd8e4
otoyo <hiroki.t.1988@gmail.com>
2023-03-07 09:23:21 +0900

    Download DB cover image

---
+++ src/integrations/cover-image-downloader.ts
@@ -0,0 +1,25 @@
+import type { AstroIntegration } from 'astro'
+import { getDatabase, downloadFile } from '../lib/notion/client'
+
+export default (): AstroIntegration => ({
+  name: 'cover-image-downloader',
+  hooks: {
+    'astro:build:start': async () => {
+      const database = await getDatabase()
+
+      if (!database.Cover || database.Cover.Type !== 'file') {
+        return Promise.resolve()
+      }
+
+      let url!: URL
+      try {
+        url = new URL(database.Cover.Url)
+      } catch (err) {
+        console.log('Invalid Cover image URL')
+        return Promise.resolve()
+      }
+
+      return downloadFile(url)
+    },
+  },
+})
