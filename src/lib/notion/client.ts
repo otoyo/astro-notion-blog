79c9194d5b818ec2d203437a43829c084e6bc5b8
otoyo <hiroki.t.1988@gmail.com>
2023-05-31 18:39:37 +0900

    Join RichText objects as well

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -861,12 +861,14 @@ function _buildPost(pageObject: responses.PageObject): Post {
       : '',
     Icon: icon,
     Cover: cover,
-    Slug: prop.Slug.rich_text ? prop.Slug.rich_text[0].plain_text : '',
+    Slug: prop.Slug.rich_text
+      ? prop.Slug.rich_text.map((richText) => richText.plain_text).join('')
+      : '',
     Date: prop.Date.date ? prop.Date.date.start : '',
     Tags: prop.Tags.multi_select ? prop.Tags.multi_select : [],
     Excerpt:
       prop.Excerpt.rich_text && prop.Excerpt.rich_text.length > 0
-        ? prop.Excerpt.rich_text.map((t) => t.plain_text).join('')
+        ? prop.Excerpt.rich_text.map((richText) => richText.plain_text).join('')
         : '',
     FeaturedImage: featuredImage,
     Rank: prop.Rank.number ? prop.Rank.number : 0,
