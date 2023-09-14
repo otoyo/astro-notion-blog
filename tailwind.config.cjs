86d6e94e5f30957898adf7c77b75e49aa479d60a
lesssummer <57969740+kelaspajak@users.noreply.github.com>
2023-09-14 13:35:44 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
