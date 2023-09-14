0af58645448f2f625485d824d147aadeba803719
lesssummer <57969740+kelaspajak@users.noreply.github.com>
2023-09-14 17:57:29 +0800

    styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        body: ["Inter", "sans-serif"],
-        heading: ["Inter", "sans-serif"],
+        body: ['Inter', 'sans-serif'],
+        heading: ['Inter', 'sans-serif'],
       },
     },
   },
