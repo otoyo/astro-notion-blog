1f2e67bdb409e29a84b08160c6afa7f52c7ed3c7
lesssummer <57969740+kelaspajak@users.noreply.github.com>
2023-09-14 09:35:11 +0800

    add inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+        heading: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
