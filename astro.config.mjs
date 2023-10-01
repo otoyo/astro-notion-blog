fce5899bdd80a0b1f3f18129d38458b604c00fef
kamico16 <kamico1636@gmail.com>
2023-10-01 14:10:58 +0900

    install tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,41 +4,27 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
-
+import tailwind from "@astrojs/tailwind";
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
-
   if (process.env.VERCEL && process.env.VERCEL_URL) {
     return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
   }
-
   if (process.env.CF_PAGES) {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
+    return new URL(BASE_PATH, `https://${new URL(process.env.CF_PAGES_URL).host.split('.').slice(1).join('.')}`).toString();
   }
-
   return new URL(BASE_PATH, 'http://localhost:3000').toString();
 };
+
 
 // https://astro.build/config
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [
-    CoverImageDownloader(),
-    CustomIconDownloader(),
-    FeaturedImageDownloader(),
-    PublicNotionCopier(),
-  ],
+  integrations: [CoverImageDownloader(), CustomIconDownloader(), FeaturedImageDownloader(), PublicNotionCopier(), tailwind()]
 });
