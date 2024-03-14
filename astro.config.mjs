5316b2cca7493dc2dcc566f3a2c3b73d8216fc5e
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2024-03-14 16:05:53 +0900

    add tailwind and fontsource modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
+import tailwind from '@astrojs/tailwind';
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
@@ -40,5 +41,6 @@ export default defineConfig({
     CustomIconDownloader(),
     FeaturedImageDownloader(),
     PublicNotionCopier(),
+    tailwind(),
   ],
 });
