9d035a11b9ac33ce22c77e42306aef4003cba0c4
URAPRO <tk.yabashi@gmail.com>
2024-05-19 15:27:45 +0900

    reactのinstall

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,32 +5,23 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
-
+import react from "@astrojs/react";
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
-
   if (process.env.VERCEL && process.env.VERCEL_URL) {
     return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
   }
-
   if (process.env.CF_PAGES) {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
+    return new URL(BASE_PATH, `https://${new URL(process.env.CF_PAGES_URL).host.split('.').slice(1).join('.')}`).toString();
   }
-
   return new URL(BASE_PATH, 'http://localhost:4321').toString();
 };
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -42,5 +33,6 @@ export default defineConfig({
     CustomIconDownloader(),
     FeaturedImageDownloader(),
     PublicNotionCopier(),
+    react(),
   ],
 });
