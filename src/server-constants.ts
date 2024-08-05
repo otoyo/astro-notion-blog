9c81fd135d4dae495946d24051d5dde7f22d5bb7
hayashi_yuki <42443262+hayashi16@users.noreply.github.com>
2024-08-05 23:46:00 +0900

    Update server-constants.ts
    新しいトップページを追加するために、新しく設定した環境変数のINDEX_PAGE_IDの設定を書き加える

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -15,3 +15,8 @@ export const REQUEST_TIMEOUT_MS = parseInt(
   10
 )
 export const ENABLE_LIGHTBOX = import.meta.env.ENABLE_LIGHTBOX
+
+  export const DATABASE_ID =
+    import.meta.env.DATABASE_ID || process.env.DATABASE_ID || ''
++ export const INDEX_PAGE_ID =
++   import.meta.env.INDEX_PAGE_ID || process.env.INDEX_PAGE_ID || ''
