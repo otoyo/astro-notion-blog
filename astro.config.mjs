3a3a2c1b296e31ced2c4080f6351a3a513b2b58c
Daisuke Nagai <tukinuketa@gmail.com>
2025-02-12 17:12:20 +0900

    .envファイルを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
+import 'dotenv/config';
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
