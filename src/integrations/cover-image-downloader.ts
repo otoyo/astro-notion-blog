43d31cd1be89adcaac7a47ef28b522a5d95faf84
Daisuke Nagai <tukinuketa@gmail.com>
2025-04-02 20:22:07 +0900

    カバー画像がビルド時に反映されない問題を解決した

--- src/integrations/cover-image-downloader.ts
+++ src/integrations/cover-image-downloader.ts
@@ -1,25 +1,30 @@
 import type { AstroIntegration } from 'astro'
-import { getDatabase, downloadFile } from '../lib/notion/client'
-
+import { getAllPosts, downloadFile } from '../lib/notion/client'
 export default (): AstroIntegration => ({
   name: 'cover-image-downloader',
   hooks: {
     'astro:build:start': async () => {
-      const database = await getDatabase()
+      const posts = await getAllPosts()
 
-      if (!database.Cover || database.Cover.Type !== 'file') {
-        return Promise.resolve()
-      }
+      await Promise.all(
+        posts.map((post) => {
+          if (!post.Cover || !post.Cover.Url) {
+            console.log('No Cover image URL: ', post.Cover?.Url)
+            return Promise.resolve()
+          }
 
-      let url!: URL
-      try {
-        url = new URL(database.Cover.Url)
-      } catch {
-        console.log('Invalid Cover image URL: ', database.Cover?.Url)
-        return Promise.resolve()
-      }
+          let url!: URL
+          try {
+            url = new URL(post.Cover.Url)
+            console.log('set Cover URL: ', post.Cover?.Url)
+          } catch {
+            console.log('Invalid Cover URL: ', post.Cover?.Url)
+            return Promise.resolve()
+          }
 
-      return downloadFile(url)
+          return downloadFile(url)
+        })
+      )
     }
   }
 })
