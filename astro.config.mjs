3ed5bf4d3fd6f2518e9d5c37a47227b2621c557f
Yaw Kai Yuan <tp080121@mail.apu.edu.my>
2025-09-25 21:58:04 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,16 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
 import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
 
-const getSite = function () {
-  if (CUSTOM_DOMAIN) {
-    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
-  }
+// Use exact GitHub Pages URL and repo-base path
+const SITE = 'https://yaw31.github.io/astro-notion-blog/';
 
-  if (process.env.VERCEL && process.env.VERCEL_URL) {
-    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
-  }
-
-  if (process.env.CF_PAGES) {
-    if (process.env.CF_PAGES_BRANCH !== 'main') {
-      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
-    }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
-  }
-
-  return new URL(BASE_PATH, 'http://localhost:4321').toString();
-};
-
-// https://astro.build/config
 export default defineConfig({
-  site: getSite(),
-  base: BASE_PATH,
+  site: SITE,
+  base: '/astro-notion-blog/',
   integrations: [
     icon(),
     CoverImageDownloader(),
